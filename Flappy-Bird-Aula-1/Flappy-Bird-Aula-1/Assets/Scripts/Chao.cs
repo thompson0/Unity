96affa34a04a05cb69159adfe949365535c4c817
Tiago Marchetti Lyra <>
2024-05-27 11:23:29 -0300

    flappy pimba

---
+++ Flappy-Bird-Aula-1/Flappy-Bird-Aula-1/Assets/Scripts/Chao.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chao : MonoBehaviour
+{
+    [SerializeField]
+    private float velocidade;
+
+    private Vector3 posicaoinicial;
+    private float tamanhoDaImagemNaCena;
+
+    private void Awake()
+    {
+        this.posicaoinicial = this.transform.position;
+        float tamanhoDaImagemReal = GetComponent<SpriteRenderer>().size.x;
+        float escala = this.transform.localScale.x;
+        this.tamanhoDaImagemNaCena = tamanhoDaImagemReal * escala;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deslocamento = Mathf.Repeat(this.velocidade * Time.time, tamanhoDaImagemNaCena);
+        this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
+
+        this.transform.position = this.posicaoinicial + Vector3.left * deslocamento;
+
+    }
+}
--- Flappy-Bird-Aula-1/Flappy-Bird-Aula-1/Assets/Scripts/Obstaculo.cs
+++ Flappy-Bird-Aula-1/Flappy-Bird-Aula-1/Assets/Scripts/Obstaculo.cs
@@ -6,10 +6,17 @@ public class Obstaculo : MonoBehaviour
 {
     [SerializeField]
     private float velocidade = 0.6f;
+    [SerializeField]
+    private float variacaoposicaoY;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void Awake()
+    {
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoposicaoY,variacaoposicaoY));
     }
 
     // Update is called once per frame
